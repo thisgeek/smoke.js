2d2d81fb0cca54ac82481c5a17d012c80752d17b
Carlos Brito Lage <carlos@carloslage.net>
2011-11-04 12:29:28 +0000

    Smoke.js now using DOMContentLoaded implementation by Diego Perini to prevent image loading from delaying availability of the smoke dialogs.

--- smoke/smoke.js
+++ smoke/smoke.js
@@ -1,6 +1,56 @@
 /*jslint browser: true, onevar: true, undef: true, nomen: false, eqeqeq: true, bitwise: true, regexp: true, newcap: true, immed: true */
 
 (function () { 
+	/*! 
+	 * contentloaded.js
+	 *
+	 * Author: Diego Perini (diego.perini at gmail.com)
+	 * Summary: cross-browser wrapper for DOMContentLoaded
+	 * Updated: 20101020
+	 * License: MIT
+	 * Version: 1.2
+	 *
+	 * URL:
+	 * http://javascript.nwbox.com/ContentLoaded/
+	 * http://javascript.nwbox.com/ContentLoaded/MIT-LICENSE
+	 *
+	 */
+	
+	// @win window reference
+	// @fn function reference
+	function contentLoaded(win, fn) {
+	
+		var done = false, top = true,
+	
+		doc = win.document, root = doc.documentElement,
+	
+		add = doc.addEventListener ? 'addEventListener' : 'attachEvent',
+		rem = doc.addEventListener ? 'removeEventListener' : 'detachEvent',
+		pre = doc.addEventListener ? '' : 'on',
+	
+		init = function(e) {
+			if (e.type === 'readystatechange' && doc.readyState !== 'complete') return;
+			(e.type === 'load' ? win : doc)[rem](pre + e.type, init, false);
+			if (!done && (done = true)) fn.call(win, e.type || e);
+		},
+	
+		poll = function() {
+			try { root.doScroll('left'); } catch(e) { setTimeout(poll, 50); return; }
+			init('poll');
+		};
+	
+		if (doc.readyState === 'complete') fn.call(win, 'lazy');
+		else {
+			if (doc.createEventObject && root.doScroll) {
+				try { top = !win.frameElement; } catch(e) { }
+				if (top) poll();
+			}
+			doc[add](pre + 'DOMContentLoaded', init, false);
+			doc[add](pre + 'readystatechange', init, false);
+			win[add](pre + 'load', init, false);
+		}
+	}
+
 	var smoke = {
 	  smoketimeout: [],
 	  init: false,
@@ -23,10 +73,10 @@
 	
 			// make a new container while you're at it
 			if (!smoke.init){		
-					smoke.listen(window,"load", function(){
+				contentLoaded(window, function() {
 					smoke.bodyload(newid);
 				});
-			}else{
+			} else {
 				smoke.bodyload(newid);		
 			}
 	
@@ -379,7 +429,7 @@
 	};
 	
 	if (!smoke.init) {		
-		smoke.listen(window, "load", function () {
+		contentLoaded(window, function() {
 			smoke.init = true;
 		});
 	}
